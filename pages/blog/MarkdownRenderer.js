33f2d12a769506a299e3587b6ddb31bcc75ad6e5
Neeraj Butola <34231063+Frostdev7506@users.noreply.github.com>
2023-08-17 23:37:13 +0530

    changed <A>to <link> tags in footer component ezzyy

--- pages/blog/MarkdownRenderer.js
+++ pages/blog/MarkdownRenderer.js
@@ -5,7 +5,7 @@ import remarkGfm from 'remark-gfm'
 const MarkdownRenderer = ({ content }) => {
   const sanitizedContent = content.replace(/\r\n/g, '\n');
   console.log("sanitizedContent",sanitizedContent);
-  return <ReactMarkdown className="custom-markdown" default={sanitizedContent} remarkPlugins={[remarkGfm]}>{sanitizedContent}</ReactMarkdown>;
+  return <ReactMarkdown className="custom-markdown" remarkPlugins={[remarkGfm]}>{sanitizedContent}</ReactMarkdown>;
 };
 
 export default MarkdownRenderer;
