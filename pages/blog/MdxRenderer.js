66914f8c1bdc1d777b79ddfa34c73ddf67055da8
Neeraj Butola <34231063+Frostdev7506@users.noreply.github.com>
2023-08-18 01:02:37 +0530

    ezzy

--- pages/blog/MdxRenderer.js
+++ pages/blog/MdxRenderer.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { run } from '@mdx-js/mdx';
+// import React, { useState, useEffect } from 'react';
+// import { run } from '@mdx-js/mdx';
 
-const MdxRenderer = ({ content }) => {
-  const [mdxComponent, setMdxComponent] = useState(null);
+// const MdxRenderer = ({ content }) => {
+//   const [mdxComponent, setMdxComponent] = useState(null);
 
-  useEffect(() => {
-    const loadMdxComponent = async () => {
-      const mdxModule = await run(content, { outputFormat: 'function-body' });
-      const Component = new Function('React', ...Object.keys(mdxModule), mdxModule['default']);
-      setMdxComponent(Component);
-    };
+//   useEffect(() => {
+//     const loadMdxComponent = async () => {
+//       const mdxModule = await run(content, { outputFormat: 'function-body' });
+//       const Component = new Function('React', ...Object.keys(mdxModule), mdxModule['default']);
+//       setMdxComponent(Component);
+//     };
 
-    loadMdxComponent();
-  }, [content]);
+//     loadMdxComponent();
+//   }, [content]);
 
-  if (!mdxComponent) {
-    return <div>Loading...</div>;
-  }
+//   if (!mdxComponent) {
+//     return <div>Loading...</div>;
+//   }
 
-  return React.createElement(mdxComponent, {});
-};
+//   return React.createElement(mdxComponent, {});
+// };
 
-export default MdxRenderer;
+// export default MdxRenderer;
--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,7 @@ import github from "../public/github.png";
 import js from "../public/javascript.png";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import Footer from '../components/Footer'; 
 
 const override = {
   margin: "0 auto",
@@ -499,49 +500,7 @@ export default function Home() {
 </section>
 
         {/* footer  */}
-        <footer className="p-4 mt-5 bg-white rounded-lg shadow-2xl md:px-6 md:py-8 dark:bg-gray-900">
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <a
-              href="https://github.com/Frostdev7506"
-              className="flex items-center mb-4 sm:mb-0"
-            >
-              <span className="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white">
-                <p className="text-xl text-gray-500 font-burtons ">
-                  developedbyneeraj
-                </p>
-              </span>
-            </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-              <li>
-                <a href="#about" className="mr-4 hover:underline md:mr-6 ">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6">
-                  Privacy Policy
-                </a>
-              </li>
-
-              <li>
-                <a href="#contactme" className="hover:underline">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            ©{" "}
-            <a
-              href="https://github.com/Frostdev7506"
-              className="hover:underline"
-            >
-              NeerajButola™
-            </a>
-            . All Rights Reserved.
-          </span>
-        </footer>
+       <Footer/>
       </main>
     </div>
   );
