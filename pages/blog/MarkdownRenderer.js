1b5da0b742792ddad7bb8c0796133b788bf8c0e8
Neeraj Butola <34231063+Frostdev7506@users.noreply.github.com>
2023-08-18 01:19:50 +0530

    solve

--- pages/blog/MarkdownRenderer.js
+++ pages/blog/MarkdownRenderer.js
@@ -1,11 +1,23 @@
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm'
-
+import remarkGfm from 'remark-gfm';
 
 const MarkdownRenderer = ({ content }) => {
+  if (!content) {
+    return null; // or some fallback content if needed
+  }
+
   const sanitizedContent = content.replace(/\r\n/g, '\n');
-  console.log("sanitizedContent",sanitizedContent);
-  return <ReactMarkdown className="custom-markdown" remarkPlugins={[remarkGfm]}>{sanitizedContent}</ReactMarkdown>;
+  console.log("sanitizedContent", sanitizedContent);
+
+  return (
+    <ReactMarkdown
+      className="custom-markdown"
+      components={{}} 
+      remarkPlugins={[remarkGfm]}
+    >
+      {sanitizedContent}
+    </ReactMarkdown>
+  );
 };
 
 export default MarkdownRenderer;
