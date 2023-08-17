17b89fc9dbbb721d873da8941d24fa1f129efc0d
Neeraj Butola <34231063+Frostdev7506@users.noreply.github.com>
2023-08-17 23:31:07 +0530

    changed <A>to <link> tags in footer component ezzy

--- components/Footer.js
+++ components/Footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <footer className="p-4 mt-5 bg-white rounded-lg shadow-2xl md:px-6 md:py-8 dark:bg-gray-900">
       <div className="sm:flex sm:items-center sm:justify-between">
-        <a href="https://github.com/Frostdev7506" className="flex items-center mb-4 sm:mb-0">
+        <Link href="https://github.com/Frostdev7506" className="flex items-center mb-4 sm:mb-0">
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white">
             <p className="text-xl text-gray-500 font-burtons">developedbyneeraj</p>
           </span>
-        </a>
+        </Link>
         <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
           <li>
             <Link href="/#about" className="mr-4 hover:underline md:mr-6">
@@ -31,9 +31,9 @@ const Footer = () => {
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         ©{" "}
-        <a href="https://github.com/Frostdev7506" className="hover:underline">
+        <Link href="https://github.com/Frostdev7506" className="hover:underline">
           NeerajButola™
-        </a>
+        </Link>
         . All Rights Reserved.
       </span>
     </footer>
